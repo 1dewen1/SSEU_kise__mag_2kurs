57d82dbf8a311cc4a71477ff1bda9f0593748dfa
1dewen1 <dewendsewes@gmail.com>
2016-10-17 23:11:36 +0400

    Лекция 2. Пример 2. Максимальное из трех чисел через операцию ?:.

--- vybor/ConsoleApplication1/Program.cs
+++ vybor/ConsoleApplication1/Program.cs
@@ -9,13 +9,13 @@ namespace vybor
         static void Main(string[] args)
         {
             //ввод исходных значений
-            int A, B;
-            if (int.TryParse(Console.ReadLine(), out A))
-            {
-                if (int.TryParse(Console.ReadLine(), out B))
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B)
+                && int.TryParse(Console.ReadLine(), out C))
                 {
                     // максимальное значение 
-                    int Max = (A >= B) ? A : B; 
+                    int Max = (A >= B) ? A : B;
+                    Max = (Max >= C) ? Max : C;
                     // вывод результата
                     Console.WriteLine("Максимальное = " + Max);
                     Console.ReadKey();
@@ -25,11 +25,7 @@ namespace vybor
                     Console.WriteLine("Неверное значение!Нужно ввести целое число.");
                     Console.ReadKey();
                 };
-            {
-                Console.WriteLine("Неверное значение! Нужно ввести целое число.");
-                Console.ReadKey();
-            };
+            
             }
         }
     }
-}
