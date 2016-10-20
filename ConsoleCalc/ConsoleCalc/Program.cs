fdd1be23e2f870e889db7882999a3cd674450570
1dewen1 <dewendsewes@gmail.com>
2016-10-20 13:48:59 +0400

    Лекция 2. Пример 4. Консольный калькулятор: сначала вводим операнды, потом операцию.

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -8,34 +8,58 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
-            double operand1; // операнды 1
-            double operand2; // операнд 2
+            double operand1 = 0; // операнды 1
+            double operand2 = 0; // операнд 2
             double result = 0; // результат вычислени€
             string operation; // действие
 
-            Console.ReadKey();
-           
             if (double.TryParse(Console.ReadLine(), out operand1) && double.TryParse(Console.ReadLine(), out operand2))
             {
-
+                // выбираем операцию
+            operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    if (double.TryParse(Console.ReadLine(), out operand2))
+                    {
+                        result = operand1 + operand2;
+                    };
+                    break;
+                case "-": 
+                    if (double.TryParse(Console.ReadLine(), out operand2))
+                    {
+                        result = operand1 - operand2;
+                    };
+                    break;
+                case "*": 
+                    if (double.TryParse(Console.ReadLine(), out operand2))
+                    {
+                        result = operand1 * operand2;
+                    };
+                    break;
+                case "/": 
+                    if (double.TryParse(Console.ReadLine(), out operand2))
+                    {
+                        result = operand1 / operand2;
+                    };
+                    break;
+                case "sqrt":
+                    if (double.TryParse(Console.ReadLine(), out operand2))
+                    {
+                        result = Math.Sqrt(operand1) ;
+                    };
+                    break;
+                default: Console.WriteLine("Ќеверна€ операци€!"); result = 0;
+                    break;
+            };
+            //выводим результат
+            Console.WriteLine(operand1 + " " + operation + " " + operand2 + " = " + result);
             }
             else
             {
                 Console.WriteLine("ќперанд должен быть числом!"); 
             };
-            // выбираем операцию
-            operation = Console.ReadLine();
-            switch (operation)
-            {
-                case "+": break;
-                case "-": break;
-                case "*": break;
-                case "/": break;
-                case "sqrt": break;
-                default: Console.WriteLine("Ќеверна€ операци€!"); result = 0; break;
-            };
-            //выводим результат
-            Console.WriteLine(operand1 + " " + operation + " " + operand2 + " = " + result);
+    
         }
     }
 }
