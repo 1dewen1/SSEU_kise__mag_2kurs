f7af7edb248fbc37a34ad6b92d9fdd886d91c024
1dewen1 <dewendsewes@gmail.com>
2016-10-20 20:02:17 +0400

    Лекция 2. Задание 1. Преобразование градусов Цельсия в градусы Фарингейта.

---
+++ CinF/CinF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // объявление переменных
+           double cel;
+           double.TryParse(Console.ReadLine(), out cel);
+           // перевод градусов Цельсия в градусы Фарингейта
+           double cinf = (cel * 1.8) + 32;
+           // вывод значения
+           Console.WriteLine(cinf + " градусов фарингейта");
+           Console.ReadKey();
+        }
+    }
+}
