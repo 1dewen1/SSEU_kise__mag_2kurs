9bf71a840d57dbc2d10eb6ebe94501a87649b240
1dewen1 <dewendsewes@gmail.com>
2016-10-20 20:52:04 +0400

    Лекция 2. Пример 3. Вывод суммы без и со скидкой.

---
+++ SaleSum/SaleSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaleSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            double sale;
+            int.TryParse(Console.ReadLine(), out sum);
+            if (sum > 1000)
+            {
+                sale = sum - sum * 15 / 100;
+                Console.WriteLine("Сумма со скидкой равна " + sale);
+                Console.ReadKey();
+            }
+            else 
+            {
+                Console.WriteLine("Сумма без скидки равна " + sum);
+                Console.ReadKey();
+            };
+       
+        }
+    }
+}
