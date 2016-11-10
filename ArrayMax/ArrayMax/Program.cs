78111682d4e4d5fa2361e02bff1973dda6f4c180
Дмитрий Меньщиков <dewendsewes@gmail.com>
2016-11-10 17:08:43 +0400

    Лекция 2.Пример 2. Поиск максимального значения в массиве.

---
+++ ArrayMax/ArrayMax/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод длины массива
+            int n;
+            do
+            { 
+                Console.Write("Введите длину массива: ");
+            } while (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
+            //инициация массива
+            int[] a = new int[n];
+            //ввод значений массива
+            int i = 0;
+            while (i < n)
+            {
+                Console.Write("Введите a[" + i + "]: ");
+                if (int.TryParse(Console.ReadLine(), out a[i]))
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Введите целое число!");
+                }; ;
+            };
+            //поиск максимального значения
+            int max = a[0];
+            for (i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                };
+            };
+            //вывод результата
+            Console.WriteLine("Максимальное значение: " + max);
+            Console.ReadKey();
+        }
+    }
+    }
+
