5fc11f02cd65182a4d4b0a0d2f5566c6d27ff7a5
Дмитрий Меньщиков <dewendsewes@gmail.com>
2016-11-11 00:03:37 +0400

    Лекция 4.Пример 2. Идентификатор.

--- Identificator/Identificator/Program.cs
+++ Identificator/Identificator/Program.cs
@@ -8,6 +8,17 @@ namespace Identificator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите идентификатор");
+            string id = Console.ReadLine(); //идентификатор
+            //проверка первого символа
+            if (!((id[0] >= 'a') && (id[0] <= 'z') //не строчная буква
+               || (id[0] >= 'A') && (id[0] <= 'Z') //или заглавная буква
+               || (id[0] == '_'))) //или подчеркивание
+            {
+                Console.WriteLine("Первый символ должен быть буквой или знаком подчеркивания!");
+                Console.ReadKey();
+            }
+
         }
     }
 }
