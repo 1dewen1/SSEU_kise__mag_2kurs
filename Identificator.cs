a99f754681bf69796da9bf08a57ce30d09d91fee
Дмитрий Меньщиков <dewendsewes@gmail.com>
2016-11-10 23:35:55 +0400

    Лекция 4.Пример 2. Идентификатор.

---
+++ Identificator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace identificator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
