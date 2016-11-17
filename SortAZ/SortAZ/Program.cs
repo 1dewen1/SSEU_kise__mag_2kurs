41e29d9d5853687992ce61f58258edcb762474bd
1dewen1 <dewendsewes@gmail.com>
2016-11-17 20:37:23 +0400

    «Лекция 4.Пример 4. Сортировка слов по алфавиту.

---
+++ SortAZ/SortAZ/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortAZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод строки
+            Console.WriteLine("¬ведите слова, разделенные пробелами:");
+            string words = Console.ReadLine();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
