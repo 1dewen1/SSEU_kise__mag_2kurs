07e9d176a7fa4cf7359287b7cedc9df1c74dbda0
1dewen1 <dewendsewes@gmail.com>
2016-11-17 20:50:08 +0400

    «Лекция 4.Пример 4. Сортировка слов по алфавиту.

--- SortAZ/SortAZ/Program.cs
+++ SortAZ/SortAZ/Program.cs
@@ -11,7 +11,17 @@ namespace SortAZ
             //ввод строки
             Console.WriteLine("¬ведите слова, разделенные пробелами:");
             string words = Console.ReadLine();
-
+            //преобразование строки в массив, разделитель - пробел
+            string[] wordsArr = words.Split(' ');
+            //сортировка массива
+            Array.Sort(wordsArr);
+            //собираем массив обратно в строку
+            words = wordsArr[0];
+            for (int i = 1; i < wordsArr.Length; i++)
+                words = words + " " + wordsArr[i];
+            //вывод результата на экран
+            Console.WriteLine("—ортировка по алфавиту");
+            Console.WriteLine(words);
 
 
 
