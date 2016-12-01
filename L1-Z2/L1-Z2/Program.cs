1505aa654aa185eab97b56c017f0bb7e0464a650
1dewen1 <dewendsewes@gmail.com>
2016-12-01 19:56:00 +0400

    Лекция 1. Пример 2. Нахождение f(x)ю

---
+++ L1-Z2/L1-Z2/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L1_Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double a;
+            double b;
+            double c;
+            Console.WriteLine("¬ведите значение x");
+            double.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("¬ведите день рождени€");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("¬ведите мес€ц дн€ рождени€");
+            double.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("¬ведите год рождени€");
+            double.TryParse(Console.ReadLine(), out c);
+            double fun = (-1*x)/a*Math.Sqrt(Math.Pow(x, 2) + b*x + c);
+            Console.WriteLine("«начение функции = " + fun);
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
