3f383c9739cf66e88eaaa4689e0c361244fbd986
Дмитрий Меньщиков <dewendsewes@gmail.com>
2016-12-03 22:47:40 +0400

    Лекция 2. Задание 4. Треугольник.

--- L2-Z4/L2-Z4/Program.cs
+++ L2-Z4/L2-Z4/Program.cs
@@ -8,7 +8,7 @@ namespace L2_Z4
     {
         static void Main(string[] args)
         {
-                        double a;
+            double a;
             double b;
             double c;
             Console.WriteLine("Введите сторону a");
@@ -20,9 +20,112 @@ namespace L2_Z4
             if ((a < b + c) && (b < a + c) && (c < a + b))
             {
                 Console.WriteLine("Треугольник является невырожденным");
-                Console.ReadKey();
+                
             }
+
+            else
+            {
+                Console.WriteLine("Треугольник является вырожденным");
+                Environment.Exit(0);
+            };
+            if (a == b && b == c && a == c)
+            {
+                Console.WriteLine("Треугольник является равносторонним");
+            }
+            else if (a == b)   
+            {
+                Console.WriteLine("Треугольник является равнобедренным");
+            }
+            else if (a == c)
+            {
+                Console.WriteLine("Треугольник является равнобедренным");
+            }
+            else if (b == c)
+            {
+                Console.WriteLine("Треугольник является равнобедренным");
+            };
+            // выявление бОльшей стороны 
+            double tCos;
+            if ((a >= b) && (a >= c))
+            {
+                // максимальное число - a
+                tCos = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)) / (2 * (b * c));
+                if (tCos > 0)
+                {
+                    Console.WriteLine("Треугольник является остроугольным");
+                }
+                else if (tCos == 0)
+                {
+                    Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    Console.WriteLine("Треугольник является тупоугольным");
+                };
+
+            }
+            else if ((b >= a) && (b >= c))
+            {
+                // максимальное число - b
+                tCos = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)) / (2 * (a * c));
+                if (tCos > 0)
+                {
+                    Console.WriteLine("Треугольник является остроугольным");
+                }
+                else if (tCos == 0)
+                {
+                    Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    Console.WriteLine("Треугольник является тупоугольным");
+                };
+            }
+            else
+            {
+                // максимальное число - c
+                tCos = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2)) / (2 * (a * c));
+                if (tCos > 0)
+                {
+                    Console.WriteLine("Треугольник является остроугольным");
+                }
+                else if (tCos == 0)
+                {
+                    Console.WriteLine("Треугольник является прямоугольным");
+                }
+                else
+                {
+                    Console.WriteLine("Треугольник является тупоугольным");
+                };
+
+            }
+            double s;
+            double p;
+            //вычисление площади и периметра
+            p = a + b + c;
+            s = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
+            if ((p > 100) && (s > 200))
+            {
+                Console.WriteLine("Большой треугольник");
+            }
+            else if ((p < 3) && (s < 1))
+            {
+                Console.WriteLine("Большой треугольник");
+            }
+            else 
+            {
+                Console.WriteLine("Обычный треугольник");
+            };
             
+
+            
+            
+            
+             
+
+
+            Console.ReadKey();
         }
+        
     }
 }
